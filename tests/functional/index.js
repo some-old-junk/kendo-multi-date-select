53bf1856494503b7d3f1aa07dcfdb480fcb91e7d
Igor Yegorov <iegoroff@gmail.com>
2016-07-19 00:35:34 +0300

    run tests

--- tests/functional/index.js
+++ tests/functional/index.js
@@ -10,28 +10,6 @@ define(function (require) {
 
         bdd.it('dummy failing test', function () {
             expect(false).to.be.true;
-        })
-
-        // bdd.describe('dummy ', function () {
-
-        //     bdd.it('should show greeting for a person name in input after form submission', function () {
-        //         return this.remote
-        //             .get(require.toUrl('index.html'))
-        //             .setFindTimeout(5000)
-        //             .findByCssSelector('body.loaded')
-        //             .findById('nameField')
-        //                 .click()
-        //                 .type('Elanie')
-        //                 .end()
-        //             .findByCssSelector('#loginForm input[type=submit]')
-        //                 .click()
-        //                 .end()
-        //             .findById('greeting')
-        //             .getVisibleText()
-        //             .then(function (text) {
-        //                 expect(text).to.be.equal('Hello, Elanie!')
-        //             }); 
-        //     });
-        // });
+        });
     });
 });
